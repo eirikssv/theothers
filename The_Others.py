310da7a42ff228574ee2257cef59e3d14a6fb843
maritimehub <eirik.starheim.svendsen@sintef.no>
2023-08-10 08:40:38 +0200

    sd

--- The_Others.py
+++ The_Others.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 from streamlit_option_menu import option_menu
-import leafmap.foliumap as leafmap
+# import leafmap.foliumap as leafmap
 from pages.mods.funcs import *
 
 
